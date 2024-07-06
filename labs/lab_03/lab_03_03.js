f96a0a09b539fc169b26334aaaa766782856aba2
ngocduong225 <myngoc2252000@gmail.com>
2024-07-06 19:36:35 +0700

    LAB3

--- labs/lab_03/lab_03_03.js
+++ labs/lab_03/lab_03_03.js
@@ -5,9 +5,9 @@
 let intArr = [12, 34, 1, 16, 28];
 
 for (let i = 0; i < intArr.length - 1; i++) {
-    for (let j = 0; j < intArr.length - 1 - i; j++) {
+    for (let j = 0; j < intArr.length - 1; j++) {
         if (intArr[j] > intArr[j + 1]) {
-            // Swap elements
+            // Hoán đổi các phần tử
             let temp = intArr[j];
             intArr[j] = intArr[j + 1];
             intArr[j + 1] = temp;
